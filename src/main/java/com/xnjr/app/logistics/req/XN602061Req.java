d87bf5c92bb480230da6d64ef39d625f0cfa8d46
luoqi14 <luoqi_14@126.com>
2016-05-30 14:29:22 +0800

    物流接口实现

---
+++ src/main/java/com/xnjr/app/logistics/req/XN602061Req.java
@@ -0,0 +1,93 @@
+package com.xnjr.app.logistics.req;
+
+public class XN602061Req {
+
+    // 货编号(选填)
+    private String code;
+
+    // 型号编号(选填)
+    private String modelCode;
+
+    // 物流单号(选填）
+    private String logisticsCode;
+
+    // 下单用户(选填）
+    private String userId;
+
+    // 第几页(必填）
+    private String start;
+
+    // 页面个数(必填）
+    private String limit;
+    
+    // 排序字段
+    private String orderColumn;
+    
+    // 排序方向
+    private String orderDir;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getModelCode() {
+		return modelCode;
+	}
+
+	public void setModelCode(String modelCode) {
+		this.modelCode = modelCode;
+	}
+
+	public String getLogisticsCode() {
+		return logisticsCode;
+	}
+
+	public void setLogisticsCode(String logisticsCode) {
+		this.logisticsCode = logisticsCode;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getStart() {
+		return start;
+	}
+
+	public void setStart(String start) {
+		this.start = start;
+	}
+
+	public String getLimit() {
+		return limit;
+	}
+
+	public void setLimit(String limit) {
+		this.limit = limit;
+	}
+
+	public String getOrderColumn() {
+		return orderColumn;
+	}
+
+	public void setOrderColumn(String orderColumn) {
+		this.orderColumn = orderColumn;
+	}
+
+	public String getOrderDir() {
+		return orderDir;
+	}
+
+	public void setOrderDir(String orderDir) {
+		this.orderDir = orderDir;
+	}
+
+}
