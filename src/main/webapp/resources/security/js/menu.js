87c1b9ef15579e75f8871d0a1352c61ed5b1be42
XIANDONG <XIANDONG@cxd>
2016-06-03 14:10:59 +0800

    BUG

--- src/main/webapp/resources/security/js/menu.js
+++ src/main/webapp/resources/security/js/menu.js
@@ -7,10 +7,6 @@ $(function() {
 	var url = $("#basePath").val() + "/menu/list";
 	doGetAjax(url, data, doSuccessPCodeBack);
 	
-<<<<<<< HEAD
-	
-=======
->>>>>>> refs/remotes/origin/master
 	// 表格初始化
 	queryTableData();
 	
